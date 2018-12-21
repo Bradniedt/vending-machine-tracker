6b9abd49ed7208b011f361bd26cdc4d6d95270fd
Brad Niedt <bradleyniedt@gmail.com>
2018-12-21 10:40:25 -0700

    adds model test for avg snack price

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -5,4 +5,15 @@ describe Machine, type: :model do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
   end
+
+  describe 'instance methods' do
+    it '.avg_snack_price' do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      snack_1 = Snack.create(name: 'twizzlers', price: 2, machines: [dons])
+      snack_2 = Snack.create(name: 'poptarts', price: 3, machines: [dons])
+
+      expect(dons.avg_snack_price).to eq(2.5)
+    end
+  end
 end
